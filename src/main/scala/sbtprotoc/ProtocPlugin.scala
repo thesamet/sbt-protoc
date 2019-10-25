ba795d28fb86b541d249db857f51abefe2296227
Nadav Samet <thesamet@gmail.com>
2019-10-25 09:57:17 -0700

    added colon

--- src/main/scala/sbtprotoc/ProtocPlugin.scala
+++ src/main/scala/sbtprotoc/ProtocPlugin.scala
@@ -241,7 +241,7 @@ object ProtocPlugin extends AutoPlugin with Compat {
         deps.flatMap { dep =>
           val set = IO.unzip(dep, extractTarget, "*.proto")
           if (set.nonEmpty)
-            streams.log.debug("Extracted from " + dep + set.mkString("\n * ", "\n * ", ""))
+            streams.log.debug("Extracted from " + dep + set.mkString(":\n * ", "\n * ", ""))
           set
         }
       }
