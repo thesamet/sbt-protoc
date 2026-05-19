4934aef698bcf96547e49e3584274c376cfcbbe6
kenji yoshida <6b656e6a69@gmail.com>
2026-05-19 17:54:26 +0900

    Delete examples/scalapb-dotty

--- examples/scalapb-dotty/build.sbt
+++
@@ -1,3 +0,0 @@
-scalaVersion := "3.0.0-M1"
-
-Compile / PB.targets := Seq(scalapb.gen() -> (Compile / sourceManaged).value / "scalapb")
--- examples/scalapb-dotty/src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-import myproto.test.TestMessage
-
-@main def hello() = {
-  val x = TestMessage(foo = Some(3))
-  println("Welcome!")
-  println(x)
-  println(x.serializedSize)
-  println(x.toProtoString)
-}
